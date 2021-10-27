a13ea0098d2da41a0d70b22d8c9ae8579ae6cbc3
NoahCothran <89223947+NoahCothran@users.noreply.github.com>
2021-10-26 19:59:11 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Main page</title>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <h1>hello there!</h1>
+    <?php echo "this is php"; ?>
+    <p>stuff about product here</p>
+    <a href="tap-to-reply/tap-mainpage.php">link to tap-to-reply page</a><br>
+    <a href="/qwxt/qwxt-mainpage.html">link to qwxt page</a>
+</body>
+
+</html>
